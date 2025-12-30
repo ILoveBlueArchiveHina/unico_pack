649edcb78b5a5bde6078a49d5bddd45f922dad1d
ILoveBlueArchiveHina <alovemartin030@gmail.com>
2025-12-31 00:47:41 +0800

    Add webcam param file and edit camera calibration launch file

--- launch/camera_calibration.launch.py
+++ launch/camera_calibration.launch.py
@@ -11,29 +11,32 @@ def generate_launch_description():
                 'video_device': '/dev/video0',
                 'image_width': 1920,
                 'image_height': 1080,
+                'pixel_format': 'mjpeg2rgb',
                 'framerate': 30.0,
-                # 'camera_info_url': 'file:///home/user/.ros/camera_info/camera.yaml'
+                'camera_info_url': 'file:///home/uni-co-jetson/ros2_ws/src/unico_pack/config/webcam_param.yaml',
+                'frame_id': "camera",
+                'camera_name': 'webcam'
             }],
             remappings=[
-                ('image_raw', 'camera/image_raw'),
-                ('camera_info', 'camera/camera_info')
+                ('image_raw', 'webcam/image_raw'),
+                ('camera_info', 'webcam/camera_info')
             ]
         ),
         
         # ArUco多標記識別節點
-        # Node(
-        #     package='aruco_ros',
-        #     executable='marker_publisher',  # 多標記版本
-        #     parameters=[{
-        #         'marker_size': 0.10,  # 10公分
-        #         'reference_frame': 'crazyflie/base_footprint/camera',
-        #         'camera_frame': 'crazyflie/base_footprint/camera',
-        #     }],
-        #     remappings=[
-        #         ('camera_info', '/camera_info'),
-        #         ('image', '/image_raw')
-        #     ]
-        # ),
+        Node(
+            package='aruco_ros',
+            executable='marker_publisher',  # 多標記版本
+            parameters=[{
+                'marker_size': 0.10,  # 10公分
+                'reference_frame': 'camera',
+                'camera_frame': 'camera',
+            }],
+            remappings=[
+                ('camera_info', 'webcam/camera_info'),
+                ('image', 'webcam/image_raw')
+            ]
+        ),
         
         # 多標記降落控制節點
         # Node(
