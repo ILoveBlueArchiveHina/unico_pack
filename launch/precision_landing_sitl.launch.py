16a59ea928a4cb0bdc13b34442b277da28b90505
ILoveBlueArchiveHina <alovemartin030@gmail.com>
2026-01-28 17:24:48 +0800

    This launch file is used for gazebo SITL simulation.

---
+++ launch/precision_landing_sitl.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # ArUco多標記識別節點
+        Node(
+            package='aruco_ros',
+            executable='marker_publisher', 
+            parameters=[{
+                'marker_size': 0.03,  # cm
+                'reference_frame': 'camera',
+                'camera_frame': 'camera',
+            }],
+            remappings=[
+                ('camera_info', 'webcam/camera_info'),
+                ('image', 'webcam/image_raw')
+            ]
+        ),
+        
+        # 多標記降落控制節點
+        Node(
+            package='unico_pack',
+            executable='multi_marker_landing_v3.py',
+        ),
+    ])
+    
